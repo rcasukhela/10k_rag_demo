f64441bd5019e306a1122d9b52ef82836f45776f
Rohan Casukhela <43920866+rcasukhela@users.noreply.github.com>
2026-05-08 15:06:34 -0400

    cache reranker in search/ endpoint

--- src/sec_rag/api.py
+++ src/sec_rag/api.py
@@ -1,11 +1,16 @@
-from fastapi import FastAPI, Query
-from sec_rag.retrieval import retrieval
+from fastapi import FastAPI
+from sec_rag.retrieval import (
+    load_reranker,
+    retrieve
+)
 
 app = FastAPI()
+
+reranker = load_reranker()
 
 @app.get("/search")
 def search(query):
     return {
         "query": query,
-        "results": retrieval(query),
+        "results": retrieve(query, reranker),
     }
